31872ab9b7197525880af6ce4586bce9ad2f9d8d
Soni-Abhishek <47699549+Soni-Abhishek@users.noreply.github.com>
2020-04-05 02:26:32 +0530

    Indention update for handlebars

--- public/js/app.js
+++ public/js/app.js
@@ -1,57 +1,34 @@
-console.log('Client side js file is loaded.')
+console.log("Client side js file is loaded.");
 
+const weatherForm = document.querySelector("form");
+const searchterm = document.querySelector("input");
+const messageOne = document.querySelector("#messageone");
+const messageTwo = document.querySelector("#messagetwo");
 
-const weatherForm = document.querySelector('form')
-const searchterm = document.querySelector('input')
-const messageOne = document.querySelector('#messageone')
-const messageTwo = document.querySelector('#messagetwo')
+weatherForm.addEventListener("submit", (e) => {
+	e.preventDefault();
+	const location = searchterm.value;
 
+	messageOne.textContent = "Loading...";
+	messageTwo.textContent = "";
 
+	if (location == "") {
+		return (messageOne.textContent = "Please provide an address.");
+	}
 
+	fetch("/weather?address=" + location).then((response) => {
+		// if(!location)
+		// {
+		//     messageOne.textContent = noaddress
+		// }
 
-
-
-weatherForm.addEventListener('submit', (e) =>{
-
-   
-    e.preventDefault()
-    const location = searchterm.value
-
-    messageOne.textContent = 'Loading...'
-    messageTwo.textContent = ''
-
-    if (location == "") {
-        return messageOne.textContent = 'Please provide an address.'
-      }
-
-
-
-
-
-    fetch('/weather?address='+ location).then((response) => {
-
-        // if(!location)
-        // {
-        //     messageOne.textContent = noaddress
-        // }
-
-
-    response.json().then((data) => {
-        
-    if (data.error) {
-    messageOne.textContent = data.error
-    }
-
-    else {
-    
-    messageOne.textContent = data.location
-    messageTwo.textContent = data.forecast
-
-    
-    }
-
-})
-}
-
-)
-})
+		response.json().then((data) => {
+			if (data.error) {
+				messageOne.textContent = data.error;
+			} else {
+				messageOne.textContent = data.location;
+				messageTwo.textContent = data.forecast;
+			}
+		});
+	});
+});
--- src/app.js
+++ src/app.js
@@ -1,148 +1,106 @@
-const path = require('path')
-const express = require('express')
-const hbs = require('hbs')
-const geocode = require('./utils/geocode')
-const forecast = require('./utils/forecast')
+const path = require("path");
+const express = require("express");
+const hbs = require("hbs");
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
 
+const app = express();
+const port = process.env.PORT || 3000;
 
-
-const app = express ()
-const port = process.env.PORT || 3000
-
-app.listen(port, ()=>{
-
-    console.log('server has started and is on port ' + port)
-})
-
-
-
+app.listen(port, () => {
+	console.log("server has started and is on port " + port);
+});
 
 //Define path for express configuration
 
-const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../pages/views')
-const partialsPath = path.join(__dirname, '../pages/partials')
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../pages/views");
+const partialsPath = path.join(__dirname, "../pages/partials");
 
+// Handlers engine and views location
 
-// Handlers engine and views location 
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
 
-app.set('view engine', 'hbs')
-app.set('views', viewsPath)
-hbs.registerPartials(partialsPath)
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath));
 
-// Setup static directory to serve 
-app.use(express.static(publicDirectoryPath))
+app.get("", (req, res) => {
+	res.render("index", {
+		title: "Weather",
+		name: "Abhishek Soni",
+		footname: "Boost",
+	});
+});
 
-app.get('', (req, res) => {
+app.get("/about", (req, res) => {
+	res.render("about", {
+		name: "Abhishek Soni",
+		title: "About",
+		location: "Hindustan",
+		footname: "Boost",
+	});
+});
 
-    res.render('index', {
-        title: 'Weather',
-        name : 'Abhishek Soni',
-        footname: 'Boost'
-    })
-})
+app.get("/help", (req, res) => {
+	res.render("help", {
+		name: "Abhishek Soni",
+		title: "Help Page",
+		message: "Stay safe at home and save your life from COVID",
+		footname: "Boost",
+	});
+});
 
-app.get ('/about', (req,res)=>{
+app.get("/weather", async (req, res) => {
+	const addressresponse = await req.query.address;
+	const noaddress = "Please provide the address!";
+	if (!addressresponse) {
+		return res.send(noaddress);
+	}
 
-    res.render('about', {
-        name: 'Abhishek Soni',
-        title:'About',
-        location: 'Hindustan',
-        footname: 'Boost'
-    })
+	console.log(req.query.address);
 
-})
+	geocode(
+		req.query.address,
+		(error, { latitude, longitude, location } = {}) => {
+			try {
+				if (error) {
+					return res.send({ error });
+				}
 
-app.get('/help',(req,res)=>{
-    res.render('help', {
-    name:'Abhishek Soni',
-    title: 'Help Page',
-    message:'Stay safe at home and save your life from COVID',
-    footname: 'Boost'
-})
-})
+				forecast(latitude, longitude, (error, outputforecast) => {
+					if (error) {
+						return res.send({ error });
+					}
 
+					res.send({
+						forecast: outputforecast,
+						location: location,
+						address: req.query.address,
+					});
+				});
+			} catch (e) {
+				res.send({ error });
+			}
+		}
+	);
+});
 
+app.get("/help/*", (req, res) => {
+	res.render("404", {
+		title: "Missing Help Page Link",
+		name: "Abhishek Soni on Help Page",
+		footname: "Boost, (obviously)",
+		error_message: "This help article might not be found.",
+	});
+});
 
-
-// app.get('/weather', (req, res)=>{
-
-//     res.send([{
-//         location: 'Mumbai',
-//         Bforecast: 'Can rain today'
-//     },
-//     {
-//         location: 'Jaipur',
-//         Bforecast: 'Wont rain today'
-//     }]
-//     )
-
-// })
-
-// app.get('/weather',(req,res) => {
-
-//     if(!req.query.address) {
-//         return res.send('Please provide an address!')
-//     }
-    
-//     console.log(req.query.address)
-    
-//     res.send([{
-//         Address : req.query.address,
-//         Message: 'We have successfully received your message'
-//     }])
-
-
-app.get('/weather', (req,res) => {
-
-if(!req.query.address) {
-    const noaddress = 'Please provide the address!'
-    return res.send(noaddress)
-}
-
-console.log(req.query.address)
-
-geocode(req.query.address, (error, {latitude, longitude, location} = {}) => {
-    if (error) {
-        return res.send({error})
-    }
-
-    forecast(latitude,longitude, (error, outputforecast) => {
-       
-        if (error) {
-           return res.send({ error })
-        }
-
-        res.send ({
-
-            forecast : outputforecast,
-            location: location,
-            address: req.query.address
-         })
-    })
-})
-
-
-})
-
-app.get('/help/*', (req,res) => {
-    res.render('404', {
-
-        title: 'Missing Help Page Link' ,
-        name:'Abhishek Soni on Help Page',
-        footname: 'Boost, (obviously)',
-        error_message : 'This help article might not be found.'
-
-    })
-})
-
-app.get('*', (req, res)=>{
-    res.render('404', {
-        title: 'Lost Page' ,
-        name:'Abhishek Soni',
-        footname: 'Boost',
-        error_message : 'Kuch to gadbad chal rahi hai daya'
-    })
-})
-
-
+app.get("*", (req, res) => {
+	res.render("404", {
+		title: "Lost Page",
+		name: "Abhishek Soni",
+		footname: "Boost",
+		error_message: "Kuch to gadbad chal rahi hai daya",
+	});
+});
