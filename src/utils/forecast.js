96a61c147d5b9337cd7249c96548160e2bf887b0
Soni-Abhishek <47699549+Soni-Abhishek@users.noreply.github.com>
2020-04-16 23:39:04 +0530

    url encode

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,34 +1,34 @@
 const request = require("request");
 
 const forecast = (latitude, longitude, callbackforecast) => {
-    const url =
-        "https://api.darksky.net/forecast/c58d4bc957d3c19f3d4d5e3cf86f9fd9/" +
-        latitude +
-        "," +
-        longitude +
-        "?units=si";
-    // &lang=hi
+	const url =
+		"https://api.darksky.net/forecast/c58d4bc957d3c19f3d4d5e3cf86f9fd9/" +
+		encodeURIComponent(latitude) +
+		"," +
+		encodeURIComponent(longitude) +
+		"?units=si";
+	// &lang=hi
 
-    request({ url, json: true }, (error, { body }) => {
-        if (error) {
-            console.log("Kuch bada error hai");
-        } else if (body.error) {
-            console.log("Location sahi nahi hai");
-        } else {
-            callbackforecast(
-                undefined,
-                "Summary : " +
-                body.daily.data[0].summary +
-                " Current temperature : " +
-                body.currently.temperature +
-                " degree celsius, and chance of rain : " +
-                body.currently.precipProbability +
-                " %, and speed of wind is : " +
-                body.currently.windSpeed +
-                "  km."
-            );
-        }
-    });
+	request({ url, json: true }, (error, { body }) => {
+		if (error) {
+			console.log("Kuch bada error hai");
+		} else if (body.error) {
+			console.log("Location sahi nahi hai");
+		} else {
+			callbackforecast(
+				undefined,
+				"Summary : " +
+					body.daily.data[0].summary +
+					" Current temperature : " +
+					body.currently.temperature +
+					" degree celsius, and chance of rain : " +
+					body.currently.precipProbability +
+					" %, and speed of wind is : " +
+					body.currently.windSpeed +
+					"  km."
+			);
+		}
+	});
 };
 
 module.exports = forecast;
