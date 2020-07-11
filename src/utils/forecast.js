6ecf876da3eb4b30c261af6943cd747ba5ed467c
abhishek-notes <61936556+abhishek-notes@users.noreply.github.com>
2020-07-11 23:11:54 +0530

    Update forecast.js

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -20,7 +20,7 @@ const forecast = (latitude, longitude, callbackforecast) => {
 				undefined,
 				"Summary : " +
 					body.daily.data[0].summary +  
-					"\r\n\t" + " Current temperature : " +
+					"\n" + " Current temperature : " +
 					body.currently.temperature +
 					" degree celsius, \r\n  and chance of rain :" +
 					body.currently.precipProbability +
